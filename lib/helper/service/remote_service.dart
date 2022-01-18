d8c538e2cc168bac6302e956f396c018cfc503d5
Priyank <priyankmaniya199@gmail.com>
2022-01-17 23:50:38 -0700

    made variable

--- lib/helper/service/remote_service.dart
+++ lib/helper/service/remote_service.dart
@@ -7,15 +7,19 @@ import 'package:paper_app/helper/model/news_model.dart';
 
 class RemoteServices {
   final TodayController newsController = Get.find<TodayController>();
-  static final String apikey = "b795d9ec49ad4d44959058bb2d211d6f";
+  static final String apikey = "JqC57c4yWyNYYn5KCzO7CGvoNo70YD0R5GPbX9hSyR";
+  static final String domain = "https://api.msn.com";
   static var client = http.Client();
   static String URL =
-      "https://api.msn.com/MSN/Feed?ocid=sm-simcast&market=en-us&query=entertainment&apikey=JqC57c4yWyNYYn5KCzO7CGvoNo70YD0R5GPbX9hSyR&\$top=10&\$skip=0&\$select=sourceid,type,url,provider,title,images,publishedDateTime";
+      "$domain/MSN/Feed?ocid=sm-simcast&market=en-us&query=entertainment&apikey=$apikey&\$top=10&\$skip=0&\$select=sourceid,type,url,provider,title,images,publishedDateTime";
 
-  static Future<Newsdata> fetchMarketNews({int page, String topic, String nextUrl}) async {
+  static Future<Newsdata> fetchMarketNews(
+      {int page, String topic, String nextUrl}) async {
     try {
       print("************ NEXTURL $topic *********** : $nextUrl");
-      var response = await client.get(Uri.parse(nextUrl != null ? nextUrl : "https://api.msn.com/MSN/Feed?ocid=sm-simcast&market=en-us&query=$topic&apikey=JqC57c4yWyNYYn5KCzO7CGvoNo70YD0R5GPbX9hSyR&\$top=10&\$skip=0&\$select=sourceid,type,url,provider,title,images,publishedDateTime"));
+      var response = await client.get(Uri.parse(nextUrl != null
+          ? nextUrl
+          : "$domain/MSN/Feed?ocid=sm-simcast&market=en-us&query=$topic&apikey=$apikey&\$top=10&\$skip=0&\$select=sourceid,type,url,provider,title,images,publishedDateTime"));
       return Newsdata.fromJson(jsonDecode(response.body));
     } catch (e) {
       print("***** ERROR *****");
